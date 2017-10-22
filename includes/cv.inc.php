c132d733a067b2cb838ef9041aadf9ba1f5f7022
Kirsty <kirsty@willsworkspace.co.uk>
2017-10-22 19:38:34 +0000

    cv in

---
+++ includes/cv.inc.php
@@ -0,0 +1,169 @@
+<div class="container-fluid" id="cv_container">
+    <div class="col-lg-12 cv_start">
+        <h1 class="cv_start_name">Kirsty Wright </h1>
+        <p>
+            Email: kirsty@evecmdr.com | Website: www.evecmdr.com | Linkedin: www.linkedin.com/in/kirstywrightweb </a>
+        </p>
+    </div>
+    <div class="col-lg-8 col-lg-offset-2 cv_section">
+        <p>
+        </p>
+    </div>
+    <div class="col-lg-12 cv_sectionDivider">
+        <h1>
+            Skills
+        </h1>
+    </div>
+    <div class="col-lg-6 col-lg-offset-3 cv_section">
+        <ul class="cv_shortPoints">
+            <li>PHP, Python3, MYSQL, HTML, CSS, Javascript, Jquery</li>
+            <li>Keen problem solver, the more complex the problem the more fun.</li>
+            <li>Writing elegant and self explaining code.</li>
+            <li>Will always endeavour to see a project through from start to finish.</li>
+            <li>Interested in the future of technology and where it will take us next.</li>
+        </ul>
+    </div>
+    <div class="col-lg-12 cv_sectionDivider">
+        <h1>
+            Experience
+        </h1>
+    </div>
+    <div class="col-lg-8 col-lg-offset-2 cv_section">
+        <h3>
+            Mintedbox Web Developer
+        </br>
+        <small>
+            July 2015 - Present
+        </small>
+    </h3>
+    <p>
+        This opportunity is being worked alongside my apprenticeship. Complex projects I have worked on include an information site for Vodafone and an internal pension calculator for Tesco. More recently I have built an internal game app for Wagamama. Whilst my role is a fullstack developer, my projects mainly consist of websites requiring bespoke systems or admin interfaces.
+    </p>
+    <ul>
+        <li>Turning PSD and PDF designs into fully fledged websites.</li>
+        <li>Building complex and secure web applications and systems.</li>
+        <li>Constantly tweaking applications to changes in client specs.</li>
+        <li>Communicating with clients regularly with updates and to receive feedback.</li>
+    </ul>
+</div>
+<div class="col-lg-12 cv_sectionDivider">
+    <h1>
+        Projects
+    </h1>
+</div>
+<div class="col-lg-8 col-lg-offset-2 cv_section">
+    <h3>
+        <img src="https://i.gyazo.com/19d2d73516eda9d051bde75772f30f91.png"/>
+    </br><small>
+        January 2016 - July 2016
+    </small>
+</h3>
+<p>
+    <a href="https://www.evecmdr.com/job/clevergirls" ><strong>Link to project</strong></a>
+</p>
+<p>
+    Clevergirls is an online gaming community and news site dedicated to promoting and informing players about online games that interest its audience. Content includes articles, podcasts, streaming and a host of other features yet to be released. I worked as the sole developer on the project and was tasked with building both the front and backends of the site. The frontend was customer facing and displayed articles and other content that was written by authors on the site. The backend contained a simple system for content authors to write articles and upload content. This was then proof read by editors who would make edits before submitting the finished pieces to the site.
+</p>
+<ul>
+    <li>Backend with multiple user levels.<ul>
+        <li>Managment of users and groups.</li>
+        <li>Author interface to create and draft articles.</li>
+        <li>Editor interface to edit and release articles.</li>
+        <li>Streamer interface to schedule streams for display in the timetable.</li>
+    </ul></li>
+    <li>Tag system for filtering articles.</li>
+    <li>Article categories to allow for podcasts and blogs.</li>
+    <li>Streamer timetable with links to twitch and other streaming services.</li>
+    <li>Commenting on articles with Disqus.</li>
+</ul>
+<h4 onClick="window.location.href = 'https://evecmdr.com'" style="text-align:center;cursor:pointer;">
+    <strong>
+        All other projects can be found at https://evecmdr.com
+    </strong>
+</h4>
+</div>
+<div class="col-lg-12 cv_sectionDivider">
+    <h1>
+        Education
+    </h1>
+</div>
+<div class="col-lg-8 col-lg-offset-2 cv_section">
+    <h3>
+        QA Apprenticeships
+    </br>
+    <small>
+        July 2015 - September 2016
+    </small>
+</h3>
+<p>
+    City &amp; Guilds Level 3 Certificate in ICT Systems and Principles </br>
+    City &amp; Guilds Level 3 Diploma in ICT Professional Competence
+</p>
+</div>
+<div class="col-lg-8 col-lg-offset-2 cv_section">
+    <h3>
+        Ashcombe School (inc SixthForm)
+    </br>
+</h3>
+<p>
+    <strong>Grades</strong>
+    <ul>
+        <li>AS - Computing - B</li>
+        <li>GCSE - Science - B</li>
+        <li>GCSE - Additional Science - B</li>
+        <li>GCSE - ICT - A</li>
+        <li>GCSE - Maths - A</li>
+        <li>GCSE - Product Design - B</li>
+        <li>GCSE - English Language - C</li>
+        <li>GCSE - English Literature - C</li>
+        <li>GCSE - History - B</li>
+        <li>GCSE - German - E</li>
+    </ul>
+</p>
+</div>
+<div class="col-lg-12 cv_sectionDivider">
+    <h1>
+        Social / Volunteer Work
+    </h1>
+</div>
+<div class="col-lg-8 col-lg-offset-2 cv_section">
+    <h3>
+        Code Club
+    </br>
+    <small>
+        St.Martins C of E Primary School
+    </br>
+    February 2015 – June 2015
+</small>
+</h3>
+<p>
+    Helped run a primary school club for children to learn how to code. 1 hour a week after school. The children are currently working with websites using the codecademy HTML and CSS course. Helping children debug and understanding web code, explaining the structure of elements and the style sheets behind websites.
+</p>
+</div>
+<div class="col-lg-8 col-lg-offset-2 cv_section">
+    <h3>
+        Ashcombe Technical Crew (Backstage manager &amp; Sound manager)
+    </br>
+    <small>
+        Ashcombe School
+    </br>
+    2012 - 2015 (July)
+</small>
+</h3>
+<p>
+    Started out as backstage crew for the yearly major production one year and quickly became one of a team of 3 who organised all the shows and events from a technical standpoint. We were known to all members of staff as the people to go to if they wanted any event planned. During sixth form we grew the team and ended up running around 15 students from all years to help with various events.
+</p>
+<p>
+    <strong>Events include :</strong>
+</p>
+<ul>
+    <li>Gym and Dance</li>
+    <li>Charities week (Student run shows every lunchtime)</li>
+    <li>Christmas carol service</li>
+    <li>Awards evening</li>
+    <li>Drama Exams (GCSE, AS and A2)</li>
+    <li>Two school shows per year, including one major school production in November</li>
+    <li>Around 4 music festivals and shows each year.</li>
+</ul>
+</div>
+</div>
--- index.php
+++ index.php
@@ -9,13 +9,26 @@ if (!isset($_GET['path']))
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link rel="stylesheet" href="/main.css">
+
+    <?php if ($_GET['path'] == 'cv'):?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <?php else:?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <?php endif;?>
+    <link rel="stylesheet" href="/main.css?r=1.1">
     <script src="/script/main.js"></script>
 </head>
+<?php
+if ($_GET['path'] == 'cv') {
+    ?>
+    <body style="padding-top:10px;">
+    <?php
+    include(__DIR__."/includes/cv.inc.php");
+} else {
+?>
 <body style="padding-top:4rem;">
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="#">Kirsty Wright</a>
@@ -28,7 +41,7 @@ if (!isset($_GET['path']))
                     <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="/cv">CV</a>
                 </li>
             </ul>
         </div>
@@ -36,5 +49,6 @@ if (!isset($_GET['path']))
     <div class="container-fluid">
         <?php include(__DIR__."/includes/".$_GET['path']).".inc.php";?>
     </div>
+    <?php } ?>
 </body>
 </html>

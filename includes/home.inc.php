f633407873f68b7cfa9fe4259ec933348a872bee
KirstyWright <kirsty@evecmdr.com>
2017-11-27 00:02:10 +0000

    proress

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -3,10 +3,9 @@
 <p class="tc">My name is Kirsty Wright and I am web developer.</p>
 <p class="tc">If you want to contact me then you can have a look at the boxes below.</p>
 <div class="tc">
-    <div class="btn custom-button-light"
-    data-toggle="tooltip" data-placement="top" title="Discord is a chat service for gamers and communities.">Discord: Kirsty#0340</div>
-    <a class="btn custom-button-light" href='mailto:kirsty@evecmdr.com'>Email: kirsty@evecmdr.com</a>
-    <a href="https://github.com/KirstyWright" class="btn custom-button-light">Github <svg version="1.1" width="16" height="16" viewBox="0 0 16 16" class="octicon octicon-mark-github" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
+    <a class="btn custom-button-light" href='mailto:kirsty@evecmdr.com' data-toggle='tooltip' data-tooltdata-placement="top" title="kirsty@evecmdr.com">Email</a>
+    <a class="btn custom-button-light" href='https://www.linkedin.com/in/kirstywrightweb/'>LinkedIn</a>
+    <a href="https://github.com/KirstyWright" class="btn custom-button-light">Github</a>
     <a class="btn custom-button-light" href='https://www.evecmdr.com/cv'>CV</a>
 </div>
 <br>
@@ -18,22 +17,108 @@
             <li>PHP</li>
             <li>Javascript (ECMA5)</li>
             <li>Mysql</li>
-            <li>Python (3)</li>
+            <li>Python (3+)</li>
         </ul>
     </div>
     <div class="col home_col">
-        <h2>Personal Projects</h2>
+        <h2>Integrations</h2>
         <ul class='language_list'>
-            <li>Polaris</li>
-            <li>Clevergirls</li>
-            <li>Entity Auth</li>
+            <li>EVE online - SSO &amp; Swagger api</li>
+            <li>Websockets</li>
+            <li>SOAP</li>
+            <li>Slack &amp; Discord (api &amp; <span data-toggle="tooltip" data-placement="top" title="'Real Time Messaging API'">RTM</span>)</li>
         </ul>
-        <a class='normal-link' href="/projects">View Projects</a>
+        <!-- <a class='normal-link' href="/projects">View Projects</a> -->
     </div>
 </div>
 <div class="big_divider"></div>
-<div class="row">
+<h1 class='tc'>Personal Projects</h1>
+<div class="big_divider"></div>
+<h2 class="tc">Clevergirls</h2>
+<p>
+    Clevergirls is an online gaming community and news site dedicated to promoting and informing players about online games that interest its audience. Content includes articles, podcasts, streaming and a host of other features yet to be released. I worked as the sole developer on the project and was tasked with building both the front and backend of the site.
+</p>
+<p>
+    The frontend was customer facing and displayed articles and other content that was written by authors on the site. The backend contained a simple system for content authors to write articles and upload content. This was then proof read by editors who would make edits before submitting the finished pieces to the site.
+</p>
+<div>
+    <img class='img_preview' style='padding:10px;float:left;display:block;margin:0 auto;height:200px;width:auto%' src="https://i.gyazo.com/2785aae1050fd31e664f47ea6c52f47e.png" alt="">
+    <img class='img_preview' style='padding:10px;float:left;display:block;margin:0 auto;height:200px;width:auto%' src="https://i.gyazo.com/40e98a18d7d184ea14baddda8cf07d08.png" alt="">
+    <img class='img_preview' style='padding:10px;float:left;display:block;margin:0 auto;height:200px;width:auto%' src="https://i.gyazo.com/a1d85d695a1c6a953d31f61d853ab791.jpg" alt="">
+    <div class="clear"></div>
+</div>
+<ul>
+    <li>Backend with multiple user levels.<ul>
+        <li>Management of users and groups.</li>
+        <li>Author interface to create and draft articles.</li>
+        <li>Editor interface to edit and release articles.</li>
+        <li>Streamer interface to schedule streams for display in the timetable.</li>
+    </ul></li>
+    <li>Tag system for filtering articles.</li>
+    <li>Article categories to allow for podcasts and blogs.</li>
+    <li>Streamer timetable with links to twitch and other streaming services.</li>
+    <li>Commenting on articles with Disqus.</li>
+</ul>
+<br>
+<div class="big_divider"></div>
+<h2 class="tc">Polaris</h2>
+<p>Polaris is a automated assistant for an intelligence network for the game EVEonline, it runs and collects information from automated relays and external sources.
+    <br>Components of Polaris include:
+</p>
+<ul>
+    <li>Desktop Python programs to collect specific program log files.</li>
+    <li>EVE single sign on and REST api integration.</li>
+    <li>Discord bots on a Python framework to output important information.</li>
+</ul>
+<br>
+<div class="big_divider"></div>
+<h2 class="tc">Entity Auth</h2>
+<p>Entity Auth is a lightweight authentication and community system for groups within EVEonline. It uses EVEs single sign on system as well as API integration. This allows groups to have an automated auth system for managing their Teamspeak and Forum roles.</p>
 
+<p>If you are looking to get Entity Auth for your organisation or group then please contact me on one of the above methods. Alternatively you can EVEmail "Princess Vamps".</p>
+
+
+<br>
+<div class="big_divider"></div>
+<h2 class="tc">A few more</h2>
+<div class="row">
+    <div class="col project_block">
+        <h3>EVE Commander</h3>
+        <img src="https://i.gyazo.com/68aabe495ed96792134c4cac614f12f6.png" style="width:100%;height:auto;"/>
+        <p>Using EVEonline's single sign on this site provides tools and methods of communication for small custom groups within the game universe.</p>
+    </div>
+    <div class="col project_block">
+        <h3>Secret Santa</h3>
+        <img src="https://i.gyazo.com/3920567227c1f6b466c047124bb80eee.png" style="width:100%;height:auto;"/>
+        <p>The second site to use EVEonline's single sign on, this microsite allowed for users to signup for a secret santa with their ingame character.</p>
+    </div>
+    <div class="col project_block">
+        <h3>Hydrophobia</h3>
+        <img src="https://i.gyazo.com/a39453399f942c83dd99b0ed00357c49.png" style="width:100%;height:auto;"/>
+        <p>A simple frontend site for another alliance of players within the EVEonline universe.</p>
+    </div>
+    <div class="col project_block">
+        <h3>Fallen Supremacy</h3>
+        <img src="https://i.gyazo.com/c6549bd0a257c7c06f65f36c4e7cb2ab.png" style="width:100%;height:auto;"/>
+        <p>This site is a simple portal to other tools and services for another group of players within EVEonline.</p>
+    </div>
 </div>
 <br>
-<?php include(__DIR__.'/home/projects.inc.php'); ?>
+
+<div class="modal fade" id='imagemodal' tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    <img src="" id="imagepreview" style='max-width:800px;'>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- script/main.js
+++ script/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip()
+    $(".img_preview").on("click", function() {
+       $('#imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
+       $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+    });
+
 });
